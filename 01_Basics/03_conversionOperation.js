d71de0a9e24b927bd75786f5dd4fec5bba82031c
ShravanThombare <shravanthombare21474@gmail.com>
2025-11-06 08:22:33 +0000

    conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,32 @@
+let score = "shravan"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber) 
+
+// "33" => 33
+// "33abc" => NaN(Not a Number) type: Number
+// true => 1: false => 0
+// Null => 0
+// undefined => NaN(Not a Number)
+
+let isLoggedIn = "shravan"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// 1 => true; 0 => false
+// "" => false
+// "Shravan" => true
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
