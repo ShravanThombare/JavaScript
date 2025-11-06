d12d2c61f52704f77ff04fbb2407bdd7fcf27788
ShravanThombare <shravanthombare21474@gmail.com>
2025-11-06 07:59:22 +0000

    datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3 + 3);  // we are using nodejs , not browser
+
+console.log(3 + 3) 
+console.log("shravan")
+let state;
+
+let name = "shravan"
+let age = 20
+let isLoggedIn = false
+
+// Premetive datatypes
+
+// 1.number => range(2 to the power 53)
+// 2.bigint
+// 3.string => ""
+// 4.boolean => true/false
+// 5.null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// Objects
+
+console.log(typeof undefined) //Output: undefined
+console.log(typeof null) // Output: object
