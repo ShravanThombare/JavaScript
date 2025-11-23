0ecd02ba9d65206f39feb215b7f4a6b51e599625
ShravanThombare <shravanthombare21474@gmail.com>
2025-11-23 09:37:31 +0000

    Object Part 2 and JSON talks

--- 02_Basics/04_Objects.js
+++ 02_Basics/04_Objects.js
@@ -57,11 +57,33 @@ const users = [
 
 users[1].email
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 
-console.log(Object.keys(tinderUser))
-console.log(Object.values(tinderUser))
-console.log(Object.entries(tinderUser))
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
 
-console.log(tinderUser.hasOwnProperty('isLogged'))
+// console.log(tinderUser.hasOwnProperty('isLogged'))
+
+
+const course ={
+    coursename: "JS in Hindi",
+    price: "999",
+    courseInstructor: "Hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+
+// {
+//     "name": "shravan",
+//     "coursename": "JS in Hindi",
+//     "price": "free"
+// }
+
